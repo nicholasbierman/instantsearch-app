e70176e31d544100641197b674dea5250a6db9cf
Nick Bierman <biermannick83@gmail.com>
2021-05-28 12:13:41 -0400

    initialize instantsearch app

--- players/index.js
+++ players/index.js
@@ -1,29 +1,29 @@
-const client = require('../algolia');
-const nbaPlayers = require('./nba-players.json');
+const client = require("../algolia");
+const nbaPlayers = require("./nba-players.json");
 
-const playersIndex = client.initIndex('nba-players');
+const playersIndex = client.initIndex("nba-players");
 
 playersIndex
   .saveObjects(nbaPlayers, {
     autoGenerateObjectIDIfNotExist: true,
   })
-//   .then(({ objectIDs }) => {
-//     console.log("PLAYER IDS");
-//     console.log(objectIDs);
-//   });
+  .then(({ objectIDs }) => {
+    console.log("PLAYER IDS");
+    console.log(objectIDs);
+  });
 
 playersIndex
   .setSettings({
     searchableAttributes: ["unordered(name)"],
   })
-//   .then(() => {
-//     playersIndex.getSettings().then((settings) => {
-//       console.log(settings);
-//     });
-//   });
+  .then(() => {
+    playersIndex.getSettings().then((settings) => {
+      console.log(settings);
+    });
+  });
 
-playersIndex.search('', {
-    filters: 'points:10 TO 20'
-}).then(({ hits }) => {
-    console.log(hits);
-});
+// playersIndex.search('', {
+//     filters: 'points:10 TO 20'
+// }).then(({ hits }) => {
+//     console.log(hits);
+// });
--- teams/index.js
+++ teams/index.js
@@ -3,29 +3,29 @@ const nbaTeams = require("./nba-teams.json");
 
 const teamsIndex = client.initIndex("nba-teams");
 
-// teamsIndex
-//   .saveObjects(nbaTeams, { autoGenerateObjectIDIfNotExist: true })
-//   .then(({ objectIDs }) => {
-//     console.log("TEAM IDS");
-//     console.log(objectIDs);
-//   });
+teamsIndex
+  .saveObjects(nbaTeams, { autoGenerateObjectIDIfNotExist: true })
+  .then(({ objectIDs }) => {
+    console.log("TEAM IDS");
+    console.log(objectIDs);
+  });
 
-// teamsIndex
-//   .setSettings({
-//     searchableAttributes: ["name", "location"], // algolia engine considers attributes higher in searchable attributes list more relevant than lower attributes
-//     customRanking: ["desc(score)"], // values can't be strings because the objects will end up being ranked alphabetically
-//   })
-//   .then(() => {
-//     teamsIndex.getSettings().then((settings) => {
-//       console.log(settings);
-//     });
-//   });
+teamsIndex
+  .setSettings({
+    searchableAttributes: ["name", "location"], // algolia engine considers attributes higher in searchable attributes list more relevant than lower attributes
+    customRanking: ["desc(score)"], // values can't be strings because the objects will end up being ranked alphabetically
+  })
+  .then(() => {
+    teamsIndex.getSettings().then((settings) => {
+      console.log(settings);
+    });
+  });
 
-teamsIndex.search('', {
-    filters: 'score:<=400'
-}).then((result) => {
-  console.log(result.nbHits);
-  for (const hit of result.hits) {
-    console.log(hit);
-  }
-});
+// teamsIndex.search('', {
+//     filters: 'score:<=400'
+// }).then((result) => {
+//   console.log(result.nbHits);
+//   for (const hit of result.hits) {
+//     console.log(hit);
+//   }
+// });
