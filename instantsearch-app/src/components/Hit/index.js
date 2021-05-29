b9f64534daf1dc278b2e9f10e883117e2cae6f1a
Nick Bierman <biermannick83@gmail.com>
2021-05-29 16:02:43 -0400

    test rendering team logo from gh-pages

--- instantsearch-app/src/components/Hit/index.js
+++ instantsearch-app/src/components/Hit/index.js
@@ -1,15 +1,27 @@
 import { Highlight } from 'react-instantsearch-dom';
+import { useSelector } from 'react-redux';
+import logo from '../../logos/atlanta_hawks.png';
+import '../../App.css';
 
 export const Hit = ({ hit }) => {
+  const indexName = useSelector(state => state.indexState.indexName);
+
   return (
     <div>
+      {indexName === "nba-teams" ? (
+        <img
+          alt={hit.name}
+          src={logo} />
+      ) : null}
       <div className="hit-name">
         <Highlight attribute="name" hit={hit} />
       </div>
-          <div className="hit-team">{hit.team}
+      <div className="hit-team">
+        {hit.team}
         <Highlight attribute="team" hit={hit} />
       </div>
-          <div className="hit-points">{hit.points} Points
+      <div className="hit-points">
+        {hit.points} Points
         <Highlight attribute="points" hit={hit} />
       </div>
     </div>
