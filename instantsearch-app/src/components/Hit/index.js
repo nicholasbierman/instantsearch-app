12b4142e35a36db915de302f16c25af9b9f6fefc
Nick Bierman <biermannick83@gmail.com>
2021-05-30 19:26:11 -0400

    add logic to render points or score depending on which index user is searching

--- instantsearch-app/src/components/Hit/index.js
+++ instantsearch-app/src/components/Hit/index.js
@@ -1,29 +1,28 @@
-import { Highlight } from 'react-instantsearch-dom';
-import { useSelector } from 'react-redux';
-import '../../App.css';
+import { Highlight } from "react-instantsearch-dom";
+import { useSelector } from "react-redux";
+import "../../App.css";
 
 export const Hit = ({ hit }) => {
-  const indexName = useSelector(state => state.indexState.indexName);
+  const indexName = useSelector((state) => state.indexState.indexName);
   const logoUrlBase = "https://instantsearch-logos.s3.amazonaws.com";
   return (
     <div>
       {indexName === "nba-teams" ? (
-        <img
-          alt={hit.name}
-          src={`${logoUrlBase}/${hit.logoUrl}`}
-        />
+        <img alt={hit.name} src={`${logoUrlBase}/${hit.logoUrl}`} />
       ) : null}
+      <div className="hit-location">
+        <Highlight attribute="location" hit={hit} />
+      </div>
       <div className="hit-name">
         <Highlight attribute="name" hit={hit} />
       </div>
-      <div className="hit-team">
-        {hit.team}
+      <div className="hit-team">{hit.team}
         <Highlight attribute="team" hit={hit} />
       </div>
-      <div className="hit-points">
-        {hit.points} Points
-        <Highlight attribute="points" hit={hit} />
-      </div>
+      {hit.points && <div className="hit-points">{hit.points} Points</div>}
+      {hit.score && (
+        <div className="hit-score">Score: {Math.ceil(hit.score)}</div>
+      )}
     </div>
   );
 };
