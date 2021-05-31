884eeed652e5c7d77e7949b9295695fcc802cde7
Nick Bierman <biermannick83@gmail.com>
2021-05-31 01:33:08 -0400

    remove unnecessary comments

--- algolia/index.js
+++ algolia/index.js
@@ -3,14 +3,4 @@ const algoliasearch = require("algoliasearch");
 const client = algoliasearch(applicationID, apiKey);
 
 
-
-
-  // ranking => points? score for teams?
-  // what does relevance actually mean?
-  /* RANKING:
-  1. number of typos
-  2. */
-
-// custom ranking
-
 module.exports = client;
